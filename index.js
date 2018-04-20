3a87014e8c98493e95ef55ff05cd9f82b17bb35f
Jeffrey V. Tolentino <jvtolentino@gmail.com>
2018-04-20 17:27:14 +0800

    course_syllabus in progress

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongojs = require('mongojs');
 
 const app = express();
 
-const db = mongojs('lms', ['syllabi']);
+const db = mongojs('lms', ['syllabi', 'topics']);
 const ObjectId = mongojs.ObjectId;
 
 // Body Parser Middleware
@@ -25,20 +25,11 @@ app.get('/syllabi_tracker', (req, res) => {
 
 
 app.get('/syllabi_tracker/mgmt', (req, res) => {
-	/*
-	db.syllabi.find( (err, docs) => {
-		res.render('syllabi_mgmt', {
-			syllabi: docs
-		});
-	});
-	*/
-
 	db.syllabi.find().sort({code: 1}).toArray((err, docs) => {
 		res.render('syllabi_mgmt', {
 			syllabi: docs
 		});
-	});
-	
+	});	
 });
 
 
@@ -47,8 +38,22 @@ app.get('/syllabi_tracker/mgmt/new_course', (req, res) => {
 });
 
 
-app.get('/syllabi_tracker/mgmt/course/edit_syllabus', (req, res) => {
-	res.render('edit_syllabus');
+app.get('/syllabi_tracker/mgmt/course/syllabus/:id', (req, res) => {
+	db.syllabi.findOne({_id: ObjectId(req.params.id)}, (err, docs1) => {
+		db.topics.find({course_id: ObjectId(req.params.id)}, (err, docs2) => {
+			res.render('course_syllabus', {
+				course: docs1,
+				topics: docs2
+			});
+		});
+	});
+});
+
+
+app.get('/syllabi_tracker/mgmt/course/new_topic/:id', (req, res) => {
+	res.render('new_topic', {
+		course_id: req.params.id
+	});
 });
 
 
@@ -67,6 +72,29 @@ app.post('/syllabi_tracker/mgmt/course/create', (req, res) => {
 		}
 	});
 });
+
+
+app.post('/syllabi_tracker/mgmt/course/topic/create', (req, res) => {
+	var newTopic = {
+		course_id: ObjectId(req.body.course_id),
+		order_number: req.body.order_number,
+		topic: req.body.topic,
+		no_of_hours: req.body.no_of_hours,
+		learning_outcomes: req.body.learning_outcomes,
+		learning_activities: req.body.learning_activities,
+		resources: req.body.resources,
+		assessment: req.body.assessment
+	}
+
+	db.topics.insert(newTopic, (err, result) => {
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect('/syllabi_tracker/mgmt/course/syllabus/'+req.body.course_id);
+		}
+	})
+});
+
 
 app.delete('/syllabi_tracker/mgmt/course/delete/:id', (req, res) => {
 	db.syllabi.remove({_id: ObjectId(req.params.id)}, (err, result) => {
